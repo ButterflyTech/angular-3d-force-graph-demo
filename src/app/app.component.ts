f4f0d3a462565b6e2b0300556a2cdb0945388eb9
LoopSun <zhouhua2297@yeah.net>
2019-08-09 14:19:20 +0800

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var ForceGraph3D: any;
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit  {
   name = 'Angular';
+
+  ngOnInit() {
+    const N = 300;
+    const gData = {
+      nodes: [...Array(N).keys()].map(i => ({ id: i })),
+      links: [...Array(N).keys()]
+        .filter(id => id)
+        .map(id => ({
+          source: id,
+          target: Math.round(Math.random() * (id-1))
+        }))
+    };
+    const Graph = ForceGraph3D()
+      (document.getElementById('3d-graph'))
+        .graphData(gData);
+  }
 }
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,27 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
-declare var ForceGraph3D: any;
 
 @Component({
   selector: 'hello',
   template: `<h1>Hello {{name}}!</h1>`,
   styles: [`h1 { font-family: Lato; }`]
 })
-export class HelloComponent implements OnInit {
+export class HelloComponent  {
   @Input() name: string;
 
-  ngOnInit() {
-    const N = 300;
-    const gData = {
-      nodes: [...Array(N).keys()].map(i => ({ id: i })),
-      links: [...Array(N).keys()]
-        .filter(id => id)
-        .map(id => ({
-          source: id,
-          target: Math.round(Math.random() * (id-1))
-        }))
-    };
-    const Graph = ForceGraph3D()
-      (document.getElementById('3d-graph'))
-        .graphData(gData);
-  }
+
 }
